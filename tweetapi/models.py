7bd47911f427d983e5c39acf1e21e03d35f6dfc9
osadolortech <brightosadolor8@gmail.com>
2022-07-05 09:41:55 +0100

    update models

--- tweetapi/models.py
+++ tweetapi/models.py
@@ -1,3 +1,26 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ProfileModel(models.Model):
+    Name = models.CharField(User,max_length=20)
+    Bio = models.CharField(max_length=60)
+    Location = models.CharField(max_length=30)
+    Birth_date = models.DateField(editable=True)
+
+class Tweet_Model(models.Model):
+    owner = models.ForeignKey(User, related_name='user')
+    tweet_text = models.CharField(max_length=168, null=False)
+    created_time = models.TimeField(auto_now_add=True)
+    retweet = models.ManyToManyField(User, related_name='retweeted_user')
+    like = models.ManyToManyField(User, related_name='like_tweet')
+    comment = models.CharField(max_length=168, null=True)
+
+    def __str__(self):
+        return self.tweet_text
+
+class CommentModels(models.Model):
+    created_time =models.TimeField(auto_now_add=True)
+    body = models.CharField(max_length=168)
+    owner = models.ForeignKey(User, related_name='comment',on_delete=CASCADE)
